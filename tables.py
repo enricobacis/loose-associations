d82653e80696cd3d7317d815c3bfbacbefafb863
enricobacis <enrico.bacis@gmail.com>
2013-11-21 23:07:42 +0100

    Add an extensible BaseGeneratedTable and some implementation using uniform and normal distribution and self-similarity function. Add attributes to Table and a method (to_indices) to convert a list of attribute names to table indices.

--- tables.py
+++ tables.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
-from random import random
 from math import log
 import sqlite3
+import random
 
 
 class Table(object):
@@ -16,19 +16,44 @@ class Table(object):
     def __getitem__(self, key):
         return self._table[key]
 
+    def to_indices(self, attrs):
+        return [self.attributes.index(a) for a in attrs]
+
 
 class SqliteTable(Table):
 
     def __init__(self, database, tablename):
         database = database if isinstance(database, sqlite3.Connection) else sqlite3.connect(database)
         database.text_factory = str
-        self._table = database.cursor().execute('SELECT * FROM %s' % tablename).fetchall()
+        cursor = database.cursor().execute('SELECT * FROM %s' % tablename)
+        self._table = cursor.fetchall()
         self._tuples = len(self._table)
+        self.attributes = zip(*cursor.description)[0]
 
 
-class RandomTable(Table):
+class BaseGeneratedTable(Table):
 
-    def __init__(self, tuples, attrs, coeff=0.5, maxvalue=1000):
+    def __init__(self, tuples, attrs, generator):
         self._tuples = tuples
-        selfsimilar = lambda: int(maxvalue * (random() ** (log(coeff) / log(1.0 - coeff))))
-        self._table = defaultdict(lambda: tuple(selfsimilar() for _ in xrange(attrs)))
+        self._table = defaultdict(lambda: tuple(generator() for _ in xrange(attrs)))
+        self.attributes = map(str, xrange(attrs))
+
+
+class RandomTable(BaseGeneratedTable):
+
+    def __init__(self, tuples, attrs, maxvalue=100):
+        BaseGeneratedTable.__init__(self, tuples, attrs, lambda: random.randint(1, maxvalue))
+
+
+class SelfSimilarTable(BaseGeneratedTable):
+
+    def __init__(self, tuples, attrs, coeff=0.5, maxvalue=100):
+        BaseGeneratedTable.__init__(self, tuples, attrs,
+            lambda: int(maxvalue * (random.random() ** (log(coeff) / log(1.0 - coeff)))))
+
+
+class GaussianTable(BaseGeneratedTable):
+
+    def __init__(self, tuples, attrs, mu=0, sigma=10):
+        BaseGeneratedTable.__init__(self, tuples, attrs,
+            lambda: int(random.gauss(mu, sigma)))
